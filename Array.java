23c7546f3b369f1fc47a7d1988ca1ca1564ddaab
forhadmia231 <forhadhossain1595@gmail.com>
2025-09-16 16:12:11 +0600

    Initial commit

---
+++ Array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        int [] arr ;
+        int arr2 [];
+        arr = new int [20];
+        //System.out.println(arr[20]);
+
+        double [] arr3 =  new double [20];
+        //System.out.println(arr3[20]);
+        System.out.println("Line 12 is execude " + arr3.length);
+
+        boolean [] arr4 = new boolean [20];
+        //System.out.println(ar
+        System.out.println("Line 13 is execude " + arr4.length);
+
+        int arr5[] = {2,2,3,4,5};
+        System.out.println("Enter array elements: " + arr5[4]);
+
+    }
+};
---
+++ number_sytem.java
@@ -0,0 +1,16 @@
+public class number_sytem {
+    public static void main(String[] args) {
+        int dec = 152;
+        int bin = 0b10011000;
+        int oct = 0230;
+        int hex = 0x98;
+
+        System.out.println("Decimal number: " + dec
+        + " Bin: " + bin
+        + " Oct: " + oct
+        + " Hex: " + hex);
+
+    };
+
+
+};
